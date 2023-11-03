ca646f11a5d133260a8650686d2076c3acca24bf
LilMartin <martin_du@qq.com>
2023-11-03 08:04:27 +0800

    ✨ feat(algo): 新增图的广度优先搜索算法

--- __test__/module/Graph.test.ts
+++ __test__/module/Graph.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from "vitest";
-import { Graph } from "../../structure";
+import { Graph, breadthFirstSearch } from "../../structure";
 
 describe("Graph Basic Test", () => {
   const graph = new Graph<string>();
@@ -19,6 +19,32 @@ describe("Graph Basic Test", () => {
     graph.addEdge("B", "F");
     graph.addEdge("E", "I");
     console.log(graph.toString());
+    expect(true).true;
+  });
+});
+
+describe("Core algo test", () => {
+  it("BreadthFisrtSearch", () => {
+    const graph = new Graph<string>();
+    const myVertices = ["A", "B", "C", "D", "E", "F", "G", "H", "I"];
+    myVertices.forEach((item) => {
+      graph.addVertex(item);
+    });
+    graph.addEdge("A", "B");
+    graph.addEdge("A", "C");
+    graph.addEdge("A", "D");
+    graph.addEdge("C", "D");
+    graph.addEdge("C", "G");
+    graph.addEdge("D", "G");
+    graph.addEdge("D", "H");
+    graph.addEdge("B", "E");
+    graph.addEdge("B", "F");
+    graph.addEdge("E", "I");
+
+    breadthFirstSearch(graph, myVertices[0], (v: string) => {
+      console.log(v);
+    });
+
     expect(true).true;
   });
 });
--- structure/module/Graph.ts
+++ structure/module/Graph.ts
@@ -1,7 +1,61 @@
+import { Queue } from "..";
 import { BaseClass } from "../common/BaseClass";
 import { Dictionary } from "./MMap";
 
-export class Graph<T> extends BaseClass {
+type GraphVerType = string | number;
+
+type ColorType = 0 | 1 | 2;
+
+const COLOR: Record<string, ColorType> = {
+  WHITE: 0,
+  GREY: 1,
+  BLACK: 2,
+};
+
+function initGraphColor<T extends GraphVerType>(
+  vertices: T[]
+): Partial<{ [key in T]: number }> {
+  const color: Partial<{ [key in T]: number }> = {};
+  for (let i = 0; i < vertices.length; i++) {
+    color[vertices[i]] = COLOR.WHITE;
+  }
+  return color;
+}
+
+export function breadthFirstSearch<T extends GraphVerType>(
+  graph: Graph<T>,
+  startVertex: T,
+  callback: (ret: T) => void
+) {
+  const vertices = graph.getVertices();
+  const adjList = graph.getAdjList();
+  const color = initGraphColor(vertices);
+
+  const queue = new Queue<T>();
+
+  queue.enqueue(startVertex);
+
+  while (!queue.isEmpty()) {
+    const u = queue.dequeue()!;
+    const neighbors = adjList.get(u); // 首先要保证所有节点都有邻居，这样才是一个完整的图
+
+    if (!neighbors) throw new Error("Not complete graph");
+    color[u] = COLOR.GREY;
+    for (let i = 0; i < neighbors.length; i++) {
+      const w = neighbors[i];
+      if (color[w] === COLOR.WHITE) {
+        color[w] = COLOR.GREY;
+        queue.enqueue(w);
+      }
+    }
+    color[u] = COLOR.BLACK;
+    if (callback) {
+      callback(u);
+    }
+  }
+}
+
+export class Graph<T extends GraphVerType> extends BaseClass {
   isDirected: boolean;
   vertices: T[];
   adjList: Dictionary<T[]>;
